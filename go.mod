0b8ac6f9578d2d4d2e8bb9df12ba198c8dc356b6
peugeot <thomaspeugeot@yahoo.fr>
2022-02-18 02:18:10 +0100

    Compilation issues Fixes #19

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/frankban/quicktest v1.13.1 // indirect
-	github.com/fullstack-lang/gong v0.0.0-20211102172351-4569cf67fcb6
+	github.com/fullstack-lang/gong v0.0.0-20220217103641-2de44f273f2f
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.7.4
--- vendor/github.com/fullstack-lang/gong/go/models/package_main.go
+++ vendor/github.com/fullstack-lang/gong/go/models/package_main.go
@@ -3,34 +3,60 @@ package models
 const PackageMain = `package main
 
 import (
-	"flag"
-	"log"
-	"os"
-	"fmt"
 	"embed"
+	"flag"
+	"fmt"
 	"io/fs"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 
 	"{{PkgPathRoot}}/controllers"
+	"{{PkgPathRoot}}/models"
 	"{{PkgPathRoot}}/orm"
+
+	{{pkgname}} "{{PkgPathAboveRoot}}"
 )
 
 var (
-	logDBFlag = flag.Bool("logDB", false, "log mode for db")
-	logGINFlag = flag.Bool("logGIN", false, "log mode for gin")
-	apiFlag   = flag.Bool("api", false, "it true, use api controllers instead of default controllers")
+	logDBFlag         = flag.Bool("logDB", false, "log mode for db")
+	logGINFlag        = flag.Bool("logGIN", false, "log mode for gin")
+	marshallOnStartup = flag.String("marshallOnStartup", "", "at startup, marshall staged data to a go file with the marshall name and '.go' (must be lowercased without spaces). If marshall arg is '', no marshalling")
+	unmarshall        = flag.String("unmarshall", "", "unmarshall data from marshall name and '.go' (must be lowercased without spaces), If unmarshall arg is '', no unmarshalling")
+	marshallOnCommit  = flag.String("marshallOnCommit", "", "on all commits, marshall staged data to a go file with the marshall name and '.go' (must be lowercased without spaces). If marshall arg is '', no marshalling")
 )
+
+// InjectionGateway is the singloton that stores all functions
+// that can set the objects the stage
+// InjectionGateway stores function as a map of names
+var InjectionGateway = make(map[string](func()))
+
+// hook marhalling to stage
+type BeforeCommitImplementation struct {
+}
+
+func (impl *BeforeCommitImplementation) BeforeCommit(stage *models.StageStruct) {
+	file, err := os.Create(fmt.Sprintf("./%s.go", *marshallOnCommit))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	defer file.Close()
+
+	models.Stage.Checkout()
+	models.Stage.Marshall(file, "{{PkgPathRoot}}/models", "main")
+}
+
 
 func main() {
 
-	
 	log.SetPrefix("{{pkgname}}: ")
 	log.SetFlags(0)
-	
+
 	// parse program arguments
 	flag.Parse()
 
@@ -46,7 +72,45 @@ func main() {
 	db := orm.SetupModels(*logDBFlag, "./test.db")
 	dbDB, err := db.DB()
 
-	// since gongsim is a multi threaded application. It is important to set up
+	// generate injection code from the stage
+	if *marshallOnStartup != "" {
+
+		if strings.Contains(*marshallOnStartup, " ") {
+			log.Fatalln(*marshallOnStartup + " must not contains blank spaces")
+		}
+		if strings.ToLower(*marshallOnStartup) != *marshallOnStartup {
+			log.Fatalln(*marshallOnStartup + " must be lowercases")
+		}
+
+		file, err := os.Create(fmt.Sprintf("./%s.go", *marshallOnStartup))
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		defer file.Close()
+
+		models.Stage.Checkout()
+		models.Stage.Marshall(file, "{{PkgPathRoot}}/models", "main")
+		os.Exit(0)
+	}
+
+	// setup the stage by injecting the code from code database
+	if *unmarshall != "" {
+		models.Stage.Checkout()
+		models.Stage.Reset()
+		models.Stage.Commit()
+		if InjectionGateway[*unmarshall] != nil {
+			InjectionGateway[*unmarshall]()
+		}
+		models.Stage.Commit()
+	}
+
+	// hook automatic marshall to go code at every commit
+	if *marshallOnCommit != "" {
+		hook := new(BeforeCommitImplementation)
+		models.Stage.OnInitCommitFromFrontCallback = hook
+	}
+
+	// since the stack can be a multi threaded application. It is important to set up
 	// only one open connexion at a time
 	if err != nil {
 		panic("cannot access DB of db" + err.Error())
@@ -56,7 +120,7 @@ func main() {
 	controllers.RegisterControllers(r)
 
 	// provide the static route for the angular pages
-	r.Use(static.Serve("/", EmbedFolder(ng, "ng/dist/ng")))
+	r.Use(static.Serve("/", EmbedFolder({{pkgname}}.NgDistNg, "ng/dist/ng")))
 	r.NoRoute(func(c *gin.Context) {
 		fmt.Println(c.Request.URL.Path, "doesn't exists, redirect on /")
 		c.Redirect(http.StatusMovedPermanently, "/")
@@ -66,9 +130,6 @@ func main() {
 	log.Printf("Server ready serve on localhost:8080")
 	r.Run()
 }
-
-//go:embed ng/dist/ng
-var ng embed.FS
 
 type embedFileSystem struct {
 	http.FileSystem
