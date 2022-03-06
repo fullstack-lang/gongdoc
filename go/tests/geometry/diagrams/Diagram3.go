b682f8572eb8f4f8c922a1b9a69b349b064ffd4d
peugeot <thomaspeugeot@yahoo.fr>
2022-03-06 17:48:17 +0100

    Add classshape delete button #27

--- go/tests/geometry/diagrams/Diagram3.go
+++ go/tests/geometry/diagrams/Diagram3.go
@@ -10,38 +10,16 @@ import (
 var Diagram3 uml.Classdiagram = uml.Classdiagram{
 	Classshapes: []*uml.Classshape{
 		{
-			Struct: &(models.Line{}),
-			Position: &uml.Position{
-				X: 310.000000,
-				Y: 120.000000,
-			},
-			Width:  240.000000,
-			Heigth: 48.000000,
-			Links: []*uml.Link{
-				{
-					Field: models.Line{}.End,
-					Middlevertice: &uml.Vertice{
-						X: 598.000000,
-						Y: 280.500000,
-					},
-					Multiplicity: "0..1",
-				},
-			},
-		},
-		{
 			Struct: &(models.Point{}),
 			Position: &uml.Position{
-				X: 166.000000,
-				Y: 233.000000,
+				X: 159.000000,
+				Y: 237.000000,
 			},
 			Width:  240.000000,
 			Heigth: 78.000000,
 			Fields: []*uml.Field{
 				{
 					Field: models.Point{}.X,
-				},
-				{
-					Field: models.Point{}.Y,
 				},
 			},
 		},
