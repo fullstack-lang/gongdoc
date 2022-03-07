9e141096460882e71e9bb744c7ae9b677c65d7d8
peugeot <thomaspeugeot@yahoo.fr>
2022-03-07 01:46:14 +0100

    Display order of fields should follow go code order Fixes #12

--- go/tests/geometry/diagrams/Diagram3.go
+++ go/tests/geometry/diagrams/Diagram3.go
@@ -12,37 +12,11 @@ var Diagram3 uml.Classdiagram = uml.Classdiagram{
 		{
 			Struct: &(models.Line{}),
 			Position: &uml.Position{
-				X: 30.000000,
-				Y: 100.000000,
+				X: 118.000000,
+				Y: 153.000000,
 			},
 			Width:  240.000000,
-			Heigth: 78.000000,
-			Links: []*uml.Link{
-				{
-					Field: models.Line{}.End,
-					Middlevertice: &uml.Vertice{
-						X: 304.500000,
-						Y: 271.000000,
-					},
-					Multiplicity: "0..1",
-				},
-				{
-					Field: models.Line{}.Start,
-					Middlevertice: &uml.Vertice{
-						X: 294.500000,
-						Y: 41.000000,
-					},
-					Multiplicity: "0..1",
-				},
-			},
-			Fields: []*uml.Field{
-				{
-					Field: models.Point{}.X,
-				},
-				{
-					Field: models.Point{}.Y,
-				},
-			},
+			Heigth: 63.000000,
 		},
 		{
 			Struct: &(models.Point{}),
--- go/tests/geometry/models/point.go
+++ go/tests/geometry/models/point.go
@@ -4,6 +4,9 @@ import "time"
 
 // A Point is a 2 dimensional coordinate (X,Y) on a plane
 type Point struct {
-	X, Y      float64
+	Z float64
+	X float64
+	Y float64
+
 	CreatedAt time.Time
 }
