5005e5bc2f3fbe27899444963ad05f6f66214efa
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-06-29 16:11:06 +0200

    Add notes #10 - added in models

---
+++ go/tests/geometry/go/models/note.go
@@ -0,0 +1,10 @@
+package models
+
+// this is an example of a note that
+// could be displayed on a diagram.
+//
+// It could explain one aspect of the model
+// for intance, describing relations between structs
+//
+//
+// gong:note note on the organization between line and points
