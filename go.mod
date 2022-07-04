9a3a33913d78f8d60b612fa74ab3e9615db47393
peugeot <thomaspeugeot@yahoo.fr>
2022-07-04 07:49:01 +0200

    Add notes Fixes #10

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/frankban/quicktest v1.13.1 // indirect
-	github.com/fullstack-lang/gong v0.0.0-20220630134524-e8fb7b10327b
+	github.com/fullstack-lang/gong v0.0.0-20220702044335-15f4f5a852a2
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.7.4
--- go/tests/geometry/go/models/note.go
+++ go/tests/geometry/go/models/note.go
@@ -1,10 +1,7 @@
 package models
 
-// this is an example of a note that
+// GONGNOTE(models organization): this is an example of a note that
 // could be displayed on a diagram.
 //
 // It could explain one aspect of the model
 // for intance, describing relations between structs
-//
-//
-// gong:note note on the organization between line and points
