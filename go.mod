fb746352f28fa49ec53d1d25d9ab19f0eeb140cb
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-09-06 14:22:39 +0200

    updated gong dep

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/frankban/quicktest v1.13.1 // indirect
-	github.com/fullstack-lang/gong v0.0.0-20220831132059-fcff635d5035
+	github.com/fullstack-lang/gong v0.0.0-20220906115540-5c4f8313bc34
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/static v0.0.1
 	github.com/gin-gonic/gin v1.7.4
--- go/tests/geometry/go/models/note.go
+++ go/tests/geometry/go/models/note.go
@@ -1,13 +1,22 @@
 package models
 
-// GONGNOTE(Note on the models): this is an example of a note that
+// GONGNOTE(Note on the models): This is an example of a note that
 // could be displayed on a diagram.
 //
 // It could explain one aspect of the model
 // for intance, describing relations between structs
 //
-// The size of the note (width and heigth) is automaticaly computed from the note
-// number of lines and max number of characters per line in the go code
+// The text of a UML note refers a comment with the GONGNOTE keyword (see example
+// for details) in the go code of the models. This follows the go code convention
+// https://pkg.go.dev/go/doc#Note
+//
+// A Note represents a marked comment starting with "MARKER(uid): note body".
+// Any note with a marker of 2 or more upper case [A-Z] letters and a uid of at least one character is recognized.
+// The ":" following the uid is optional. Notes are collected in the Package.Notes map indexed by the notes marker.
+//
+// In the UML diagram, the size of the note is automaticaly computed from the note
+// number of lines (for the width) and the number of characters per line (for the height)
+// in the go code
 //
 
 // GONGNOTE(Short note on the models): this is an of a short note
