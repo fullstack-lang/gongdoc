341ea5fa596b856cd483d15db15696ba2cdc9cd3
peugeot <thomaspeugeot@yahoo.fr>
2022-10-18 01:36:23 +0200

    Integrate gongmattree instead of bespoke side-gong-diagrams component #71 - refactoring

--- go/models/fill_up_node_tree.go
+++ go/models/fill_up_node_tree.go
@@ -2,45 +2,9 @@ package models
 
 import (
 	"log"
-
-	gong_models "github.com/fullstack-lang/gong/go/models"
 )
 
-func FillUpNodeTree(pkgelt *Pkgelt) {
-
-	// set up the gongTree to display elements
-	gongTree := (&Tree{Name: "gong", Type: TREE_OF_SYMBOLS}).Stage()
-	gongstructRootNode := (&Node{Name: "gongstructs"}).Stage()
-	gongstructRootNode.IsExpanded = true
-	gongTree.RootNodes = append(gongTree.RootNodes, gongstructRootNode)
-	for gongStruct := range *gong_models.GetGongstructInstancesSet[gong_models.GongStruct]() {
-
-		node := (&Node{Name: gongStruct.Name}).Stage()
-		node.HasCheckboxButton = true
-		gongstructRootNode.Children = append(gongstructRootNode.Children, node)
-
-		for _, field := range gongStruct.Fields {
-			node2 := (&Node{Name: field.GetName()}).Stage()
-			node2.HasCheckboxButton = true
-			node.Children = append(node.Children, node2)
-		}
-	}
-
-	gongenumRootNode := (&Node{Name: "gongenums"}).Stage()
-	gongenumRootNode.IsExpanded = true
-	gongTree.RootNodes = append(gongTree.RootNodes, gongenumRootNode)
-	for gongEnum := range *gong_models.GetGongstructInstancesSet[gong_models.GongEnum]() {
-
-		node := (&Node{Name: gongEnum.Name}).Stage()
-		node.HasCheckboxButton = true
-		gongenumRootNode.Children = append(gongenumRootNode.Children, node)
-
-		for _, value := range gongEnum.GongEnumValues {
-			node2 := (&Node{Name: value.GetName()}).Stage()
-			node2.HasCheckboxButton = true
-			node.Children = append(node.Children, node2)
-		}
-	}
+func FillUpDiagramNodeTree(pkgelt *Pkgelt) {
 
 	// generate tree of diagrams
 	gongdocTree := (&Tree{Name: "gongdoc", Type: TREE_OF_DIAGRAMS}).Stage()
@@ -49,6 +13,8 @@ func FillUpNodeTree(pkgelt *Pkgelt) {
 	classdiagramsRootNode := (&Node{Name: "class diagrams", Type: ROOT_OF_CLASS_DIAGRAMS}).Stage()
 	classdiagramsRootNode.IsExpanded = true
 	gongdocTree.RootNodes = append(gongdocTree.RootNodes, classdiagramsRootNode)
+
+	// add one node per class diagram
 	for classdiagram := range *GetGongstructInstancesSet[Classdiagram]() {
 		node := (&Node{Name: classdiagram.Name}).Stage()
 		node.Classdiagram = classdiagram
@@ -56,9 +22,13 @@ func FillUpNodeTree(pkgelt *Pkgelt) {
 		node.HasCheckboxButton = true
 		classdiagramsRootNode.Children = append(classdiagramsRootNode.Children, node)
 	}
+
+	// root of state diagrams
 	stateDiagramssRootNode := (&Node{Name: "state diagrams", Type: ROOT_OF_STATE_DIAGRAMS}).Stage()
 	stateDiagramssRootNode.IsExpanded = true
 	gongdocTree.RootNodes = append(gongdocTree.RootNodes, stateDiagramssRootNode)
+
+	// add one node per state diagram
 	for statediagram := range *GetGongstructInstancesSet[Umlsc]() {
 		node := (&Node{Name: statediagram.Name}).Stage()
 		node.Umlsc = statediagram
@@ -67,13 +37,19 @@ func FillUpNodeTree(pkgelt *Pkgelt) {
 		stateDiagramssRootNode.Children = append(stateDiagramssRootNode.Children, node)
 	}
 
-	Stage.Commit()
-	log.Printf("Parse found %d diagrams\n", len(pkgelt.Classdiagrams))
-	log.Printf("Server ready to serve on http://localhost:8080/")
-
-	// get callbacks on node updates
+	// set callbacks on node updates
 	onNodeCallbackStruct := new(CallbacksSingloton)
 	onNodeCallbackStruct.ClassdiagramsRootNode = classdiagramsRootNode
 	onNodeCallbackStruct.StateDiagramsRootNode = stateDiagramssRootNode
 	Stage.OnAfterNodeUpdateCallback = onNodeCallbackStruct
+}
+
+func FillUpNodeTree(pkgelt *Pkgelt) {
+
+	FillUpCodeNodeTree(pkgelt)
+	FillUpDiagramNodeTree(pkgelt)
+
+	Stage.Commit()
+	log.Printf("Parse found %d diagrams\n", len(pkgelt.Classdiagrams))
+	log.Printf("Server ready to serve on http://localhost:8080/")
 }
