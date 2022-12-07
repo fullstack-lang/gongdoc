1a68737125d3e2486cfcc921c01b57ac600b70e6
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2022-12-07 15:25:28 +0100

    see gong issue 226

--- ng/projects/embed.go
+++ ng/projects/embed.go
@@ -5,5 +5,5 @@ import "embed"
 // Projects is the export of the angular source code. This allows for go vendoring to import ng sources
 // and align go source and ng source of the gong stack
 //
-//go:embed all:*
+//go:embed all:gongdoc all:gongdocdiagrams
 var Projects embed.FS
