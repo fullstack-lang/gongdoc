61df3ada8a79ef9aa04338ebb8c5d27d52b8d146
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-01-04 15:39:52 +0100

    Develop a a function that does it all #85 - factoring of parameters

--- go/tests/geometry/go/cmd/geometry/main.go
+++ go/tests/geometry/go/cmd/geometry/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	"path/filepath"
 
 	gong_fullstack "github.com/fullstack-lang/gong/go/fullstack"
@@ -8,7 +9,8 @@ import (
 
 	// for diagrams
 	gongdoc_fullstack "github.com/fullstack-lang/gongdoc/go/fullstack"
-	"github.com/fullstack-lang/gongdoc/go/models"
+
+	// this is not the models of geometry (no gongc on geometry)
 
 	gongdoc_models "github.com/fullstack-lang/gongdoc/go/models"
 
@@ -20,15 +22,21 @@ import (
 
 func main() {
 
+	// paramters
 	var embeddedDiagrams *bool
+	var map_StructName_InstanceNb *map[string]int
 
 	r := gin.Default()
 	r.Use(cors.Default())
+	var goSourceDirectories embed.FS
+	goSourceDirectories = geometry.GoDir
+
+	stackName := "geometry"
 
 	// Analyse package
 	gong_fullstack.Init(r)
 	gongdoc_fullstack.Init(r)
-	modelPackage, _ := gong_models.LoadEmbedded(geometry.GoDir)
+	modelPackage, _ := gong_models.LoadEmbedded(goSourceDirectories)
 
 	// create the diagrams
 	// prepare the model views
@@ -36,23 +44,21 @@ func main() {
 
 	// first, get all gong struct in the model
 	for gongStruct := range gong_models.Stage.GongStructs {
-
 		// let create the gong struct in the gongdoc models
 		// and put the numbre of instances
 		reference := (&gongdoc_models.Reference{Name: gongStruct.Name}).Stage()
 		reference.Type = gongdoc_models.REFERENCE_GONG_STRUCT
-		nbInstances, ok := models.Stage.Map_GongStructName_InstancesNb[gongStruct.Name]
+
+		// nbInstances, ok := models.Stage.Map_GongStructName_InstancesNb[gongStruct.Name]
+		nbInstances, ok := (*map_StructName_InstanceNb)[gongStruct.Name]
 		if ok {
 			reference.NbInstances = nbInstances
 		}
 	}
-
 	if *embeddedDiagrams {
-		diagramPackage, _ = gongdoc_models.LoadEmbedded(geometry.GoDir, modelPackage)
+		diagramPackage, _ = gongdoc_models.LoadEmbedded(goSourceDirectories, modelPackage)
 	} else {
 		diagramPackage, _ = gongdoc_models.Load(filepath.Join("../../diagrams"), modelPackage, true)
 	}
-
-	diagramPackage.GongModelPath = "geometry/go/models"
-
+	diagramPackage.GongModelPath = stackName + "/go/models"
 }
