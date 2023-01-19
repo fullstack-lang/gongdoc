5f1a56c8abb7e55431584691a689a042f6331563
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-01-19 10:25:17 +0100

    With initialy no diagrams, Marshalled diagrams have no alias nor path Fixes #94 - bug reproduction

--- go/tests/geometry/go/cmd/geometry/main.go
+++ go/tests/geometry/go/cmd/geometry/main.go
@@ -2,34 +2,73 @@ package main
 
 import (
 	"embed"
-
-	gongdoc_load "github.com/fullstack-lang/gongdoc/go/load"
+	"flag"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+
+	"github.com/fullstack-lang/gongdoc"
+
+	gongdoc_load "github.com/fullstack-lang/gongdoc/go/load"
 
 	geometry "github.com/fullstack-lang/gongdoc/go/tests/geometry"
 )
 
-func main() {
+var (
+	embeddedDiagrams = flag.Bool("embeddedDiagrams", false, "parse/analysis go/models and go/embeddedDiagrams")
+	port             = flag.Int("port", 8080, "port server")
+)
 
-	// paramters
-	var embeddedDiagrams bool
+type embedFileSystem struct {
+	http.FileSystem
+}
+
+func (e embedFileSystem) Exists(prefix string, path string) bool {
+	_, err := e.Open(path)
+	return err == nil
+}
+
+func EmbedFolder(fsEmbed embed.FS, targetPath string) static.ServeFileSystem {
+	fsys, err := fs.Sub(fsEmbed, targetPath)
+	if err != nil {
+		panic(err)
+	}
+	return embedFileSystem{
+		FileSystem: http.FS(fsys),
+	}
+}
+
+func main() {
 
 	map_StructName_InstanceNb := make(map[string]int)
 	r := gin.Default()
 	r.Use(cors.Default())
-	var goSourceDirectories embed.FS
-	goSourceDirectories = geometry.GoDir
 
 	stackName := "geometry"
-	embeddedDiagrams = true
 
 	gongdoc_load.Load(
 		stackName,
-		goSourceDirectories,
+		geometry.GoDir,
 		r,
-		embeddedDiagrams,
+		*embeddedDiagrams,
 		&map_StructName_InstanceNb)
 
+	r.Use(static.Serve("/", EmbedFolder(gongdoc.NgDistNg, "ng/dist/ng")))
+	r.NoRoute(func(c *gin.Context) {
+		fmt.Println(c.Request.URL.Path, "doesn't exists, redirect on /")
+		c.Redirect(http.StatusMovedPermanently, "/")
+		c.Abort()
+	})
+
+	log.Printf("Server ready to serve on http://localhost:" + strconv.Itoa(*port) + "/")
+	err := r.Run(":" + strconv.Itoa(*port))
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
