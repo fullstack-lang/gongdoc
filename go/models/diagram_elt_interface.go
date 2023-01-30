b92a1e38a3bf8900cea4ca8729a8281d572e425f
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-01-30 11:45:50 +0100

    Replace map_Identifier_Node by pointer to Node #125 - added DiagrameltInterface

---
+++ go/models/diagram_elt_interface.go
@@ -0,0 +1,5 @@
+package models
+
+type DiagramEltInterface interface {
+	diagramElt()
+}
--- go/models/node_impl.go
+++ go/models/node_impl.go
@@ -3,4 +3,8 @@ package models
 type NodeImpl struct {
 	node   *Node
 	nodeCb *NodeCB
+
+	// if a gong object has a corresponding elt in the diagram
+	// DiagramEltInterface points to the diagram elt
+	DiagramEltInterface
 }
