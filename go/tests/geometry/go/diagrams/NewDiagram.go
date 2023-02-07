ada77a6a51f234d12e5ed8363a86b76d809b1f9c
peugeot <thomaspeugeot@yahoo.fr>
2023-02-08 00:39:46 +0100

    Add NoteShapeLink type for field of DocLink that are pointers or field of pointers Fixes #106 - preparation

---
+++ go/tests/geometry/go/diagrams/NewDiagram.go
@@ -0,0 +1,237 @@
+package diagrams
+
+import (
+	"time"
+
+	"github.com/fullstack-lang/gongdoc/go/models"
+
+	// injection point for ident package import declaration
+	ref_models "github.com/fullstack-lang/gongdoc/go/tests/geometry/go/models"
+)
+
+// generated in order to avoid error in the package import
+// if there are no elements in the stage to marshall
+var ___dummy__Stage_NewDiagram models.StageStruct
+var ___dummy__Time_NewDiagram time.Time
+
+// Injection point for meta package dummy declaration
+var ___dummy__ref_models_NewDiagram ref_models.StageStruct
+
+// currently, DocLink renaming is not enabled in gopls
+// the following map are devised to overcome this limitation
+// those maps and the processing code will be eleminated when
+// DocLink renaming will be enabled in gopls
+// [Corresponding Issue](https://github.com/golang/go/issues/57559)
+//
+// When parsed, those maps will help with the renaming process
+var map_DocLink_Identifier_NewDiagram map[string]any = map[string]any{
+	// injection point for docLink to identifiers
+
+	"ref_models.AFTER": ref_models.AFTER,
+
+	"ref_models.BEFORE": ref_models.BEFORE,
+
+	"ref_models.CONTINUOUS": ref_models.CONTINUOUS,
+
+	"ref_models.CONTINUOUS_ZERO": ref_models.CONTINUOUS_ZERO,
+
+	"ref_models.DOTTED": ref_models.DOTTED,
+
+	"ref_models.DOTTED_ONE": ref_models.DOTTED_ONE,
+
+	"ref_models.Line": &(ref_models.Line{}),
+
+	"ref_models.Line.CreationDate": (ref_models.Line{}).CreationDate,
+
+	"ref_models.Line.End": (ref_models.Line{}).End,
+
+	"ref_models.Line.JourneyTime": (ref_models.Line{}).JourneyTime,
+
+	"ref_models.Line.Name": (ref_models.Line{}).Name,
+
+	"ref_models.Line.Start": (ref_models.Line{}).Start,
+
+	"ref_models.Line.Type": (ref_models.Line{}).Type,
+
+	"ref_models.Line.VeryLongLongLongLongLongLongField": (ref_models.Line{}).VeryLongLongLongLongLongLongField,
+
+	"ref_models.LineTypeInt": ref_models.LineTypeInt(0),
+
+	"ref_models.LineTypeString": ref_models.LineTypeString(""),
+
+	"ref_models.LongNodeOnModels": ref_models.LongNodeOnModels,
+
+	"ref_models.Point": &(ref_models.Point{}),
+
+	"ref_models.Point.CreatedAt": (ref_models.Point{}).CreatedAt,
+
+	"ref_models.Point.Name": (ref_models.Point{}).Name,
+
+	"ref_models.Point.X": (ref_models.Point{}).X,
+
+	"ref_models.Point.Y": (ref_models.Point{}).Y,
+
+	"ref_models.Point.Z": (ref_models.Point{}).Z,
+
+	"ref_models.PointExclusiveSet": &(ref_models.PointExclusiveSet{}),
+
+	"ref_models.PointExclusiveSet.Name": (ref_models.PointExclusiveSet{}).Name,
+
+	"ref_models.PointExclusiveSet.Points": (ref_models.PointExclusiveSet{}).Points,
+
+	"ref_models.PointNonExclusiveSet": &(ref_models.PointNonExclusiveSet{}),
+
+	"ref_models.PointNonExclusiveSet.Name": (ref_models.PointNonExclusiveSet{}).Name,
+
+	"ref_models.PointNonExclusiveSet.Points": (ref_models.PointNonExclusiveSet{}).Points,
+
+	"ref_models.PointUse": &(ref_models.PointUse{}),
+
+	"ref_models.PointUse.Name": (ref_models.PointUse{}).Name,
+
+	"ref_models.PointUse.Points": (ref_models.PointUse{}).Points,
+
+	"ref_models.ShortNodeOnModels": ref_models.ShortNodeOnModels,
+
+	"ref_models.SimulationStage": ref_models.SimulationStage(""),
+}
+
+// init might be handy if one want to have the data embedded in the binary
+// but it has to properly reference the Injection gateway in the main package
+// func init() {
+// 	_ = __Dummy_time_variable
+// 	InjectionGateway["NewDiagram"] = NewDiagramInjection
+// }
+
+// NewDiagramInjection will stage objects of database "NewDiagram"
+func NewDiagramInjection() {
+
+	// Declaration of instances to stage
+
+	// Declarations of staged instances of Classdiagram
+	__Classdiagram__000000_NewDiagram := (&models.Classdiagram{Name: `NewDiagram`}).Stage()
+
+	// Declarations of staged instances of DiagramPackage
+
+	// Declarations of staged instances of Field
+	__Field__000000_Name := (&models.Field{Name: `Name`}).Stage()
+
+	// Declarations of staged instances of GongEnumShape
+
+	// Declarations of staged instances of GongEnumValueEntry
+
+	// Declarations of staged instances of GongStructShape
+	__GongStructShape__000000_NewDiagram_Line := (&models.GongStructShape{Name: `NewDiagram-Line`}).Stage()
+	__GongStructShape__000001_NewDiagram_Point := (&models.GongStructShape{Name: `NewDiagram-Point`}).Stage()
+
+	// Declarations of staged instances of Link
+
+	// Declarations of staged instances of Node
+
+	// Declarations of staged instances of NoteShape
+	__NoteShape__000000_ShortNodeOnModels := (&models.NoteShape{Name: `ShortNodeOnModels`}).Stage()
+
+	// Declarations of staged instances of NoteShapeLink
+	__NoteShapeLink__000000_Line := (&models.NoteShapeLink{Name: `Line`}).Stage()
+
+	// Declarations of staged instances of Position
+	__Position__000000_Pos_NewDiagram_Line := (&models.Position{Name: `Pos-NewDiagram-Line`}).Stage()
+	__Position__000001_Pos_NewDiagram_Point := (&models.Position{Name: `Pos-NewDiagram-Point`}).Stage()
+
+	// Declarations of staged instances of Tree
+
+	// Declarations of staged instances of UmlState
+
+	// Declarations of staged instances of Umlsc
+
+	// Declarations of staged instances of Vertice
+
+	// Setup of values
+
+	// Classdiagram values setup
+	__Classdiagram__000000_NewDiagram.Name = `NewDiagram`
+	__Classdiagram__000000_NewDiagram.IsInDrawMode = true
+
+	// Field values setup
+	__Field__000000_Name.Name = `Name`
+	
+	// comment added to overcome the problem with the comment map association
+
+	//gong:ident [ref_models.Line.Name]
+	__Field__000000_Name.Identifier = `ref_models.Line.Name`
+	__Field__000000_Name.FieldTypeAsString = ``
+	__Field__000000_Name.Structname = `Line`
+	__Field__000000_Name.Fieldtypename = `string`
+
+	// GongStructShape values setup
+	__GongStructShape__000000_NewDiagram_Line.Name = `NewDiagram-Line`
+	
+	// comment added to overcome the problem with the comment map association
+
+	//gong:ident [ref_models.Line]
+	__GongStructShape__000000_NewDiagram_Line.Identifier = `ref_models.Line`
+	__GongStructShape__000000_NewDiagram_Line.ShowNbInstances = false
+	__GongStructShape__000000_NewDiagram_Line.NbInstances = 0
+	__GongStructShape__000000_NewDiagram_Line.Width = 240.000000
+	__GongStructShape__000000_NewDiagram_Line.Heigth = 78.000000
+	__GongStructShape__000000_NewDiagram_Line.IsSelected = false
+
+	// GongStructShape values setup
+	__GongStructShape__000001_NewDiagram_Point.Name = `NewDiagram-Point`
+	
+	// comment added to overcome the problem with the comment map association
+
+	//gong:ident [ref_models.Point]
+	__GongStructShape__000001_NewDiagram_Point.Identifier = `ref_models.Point`
+	__GongStructShape__000001_NewDiagram_Point.ShowNbInstances = false
+	__GongStructShape__000001_NewDiagram_Point.NbInstances = 0
+	__GongStructShape__000001_NewDiagram_Point.Width = 240.000000
+	__GongStructShape__000001_NewDiagram_Point.Heigth = 63.000000
+	__GongStructShape__000001_NewDiagram_Point.IsSelected = false
+
+	// NoteShape values setup
+	__NoteShape__000000_ShortNodeOnModels.Name = `ShortNodeOnModels`
+	
+	// comment added to overcome the problem with the comment map association
+
+	//gong:ident [ref_models.ShortNodeOnModels]
+	__NoteShape__000000_ShortNodeOnModels.Identifier = `ref_models.ShortNodeOnModels`
+	__NoteShape__000000_ShortNodeOnModels.Body = `this is an example of a short note
+It uses the DocLink convention for referencing Identifiers
+In this case [Line], [Point] and [Line.Start]
+`
+	__NoteShape__000000_ShortNodeOnModels.X = 450.000000
+	__NoteShape__000000_ShortNodeOnModels.Y = 80.000000
+	__NoteShape__000000_ShortNodeOnModels.Width = 240.000000
+	__NoteShape__000000_ShortNodeOnModels.Heigth = 63.000000
+	__NoteShape__000000_ShortNodeOnModels.Matched = false
+
+	// NoteShapeLink values setup
+	__NoteShapeLink__000000_Line.Name = `Line`
+	
+	// comment added to overcome the problem with the comment map association
+
+	//gong:ident [ref_models.ShortNodeOnModels.Line]
+	__NoteShapeLink__000000_Line.Identifier = `ref_models.ShortNodeOnModels.Line`
+
+	// Position values setup
+	__Position__000000_Pos_NewDiagram_Line.X = 300.000000
+	__Position__000000_Pos_NewDiagram_Line.Y = 250.000000
+	__Position__000000_Pos_NewDiagram_Line.Name = `Pos-NewDiagram-Line`
+
+	// Position values setup
+	__Position__000001_Pos_NewDiagram_Point.X = 90.000000
+	__Position__000001_Pos_NewDiagram_Point.Y = 100.000000
+	__Position__000001_Pos_NewDiagram_Point.Name = `Pos-NewDiagram-Point`
+
+	// Setup of pointers
+	__Classdiagram__000000_NewDiagram.GongStructShapes = append(__Classdiagram__000000_NewDiagram.GongStructShapes, __GongStructShape__000000_NewDiagram_Line)
+	__Classdiagram__000000_NewDiagram.GongStructShapes = append(__Classdiagram__000000_NewDiagram.GongStructShapes, __GongStructShape__000001_NewDiagram_Point)
+	__Classdiagram__000000_NewDiagram.NoteShapes = append(__Classdiagram__000000_NewDiagram.NoteShapes, __NoteShape__000000_ShortNodeOnModels)
+	__GongStructShape__000000_NewDiagram_Line.Position = __Position__000000_Pos_NewDiagram_Line
+	__GongStructShape__000000_NewDiagram_Line.Fields = append(__GongStructShape__000000_NewDiagram_Line.Fields, __Field__000000_Name)
+	__GongStructShape__000001_NewDiagram_Point.Position = __Position__000001_Pos_NewDiagram_Point
+	__NoteShape__000000_ShortNodeOnModels.NoteShapeLinks = append(__NoteShape__000000_ShortNodeOnModels.NoteShapeLinks, __NoteShapeLink__000000_Line)
+}
+
+
