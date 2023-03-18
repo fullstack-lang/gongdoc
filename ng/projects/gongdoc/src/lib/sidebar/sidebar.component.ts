6735502af68078801a5f14b4ebcb44c9d2c63415
peugeot <thomaspeugeot@yahoo.fr>
2023-03-18 18:03:38 +0100

    pre flight in htttp GET is not working on macbook 8080 Fixes #158 - simplification

--- ng/projects/gongdoc/src/lib/sidebar/sidebar.component.ts
+++ ng/projects/gongdoc/src/lib/sidebar/sidebar.component.ts
@@ -183,7 +183,6 @@ export class SidebarComponent implements OnInit {
   constructor(
     private router: Router,
     private frontRepoService: FrontRepoService,
-    private commitNbFromBackService: CommitNbFromBackService,
     private gongstructSelectionService: GongstructSelectionService,
 
     // insertion point for per struct service declaration
@@ -1577,13 +1576,6 @@ export class SidebarComponent implements OnInit {
         }
       )
     });
-
-    // fetch the number of commits
-    this.commitNbFromBackService.getCommitNbFromBack().subscribe(
-      commitNbFromBack => {
-        this.commitNbFromBack = commitNbFromBack
-      }
-    )
   }
 
   /**
@@ -1629,11 +1621,11 @@ export class SidebarComponent implements OnInit {
   setEditorRouterOutlet(path: string) {
     let outletName = this.routeService.getEditorOutlet(this.GONG__StackPath)
     let fullPath = this.routeService.getPathRoot() + "-" + path.toLowerCase()
-    
-    let outletConf : any = {}
+
+    let outletConf: any = {}
     outletConf[outletName] = [fullPath, this.GONG__StackPath]
-    
-    this.router.navigate([ { outlets: outletConf } ]);
+
+    this.router.navigate([{ outlets: outletConf }]);
   }
 
   setEditorSpecialRouterOutlet(node: GongFlatNode) {
