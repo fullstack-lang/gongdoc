c9628e56ea837333b1ab10853471e74683bf47e7
peugeot <thomaspeugeot@yahoo.fr>
2023-03-20 08:54:25 +0100

    back to before intrusmentation

--- ng/projects/gongdoc/src/lib/gongdoc.module.ts
+++ ng/projects/gongdoc/src/lib/gongdoc.module.ts
@@ -4,9 +4,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { LoggingInterceptorService } from './logging-interceptor.service';
-
 // for angular material
 import { MatSliderModule } from '@angular/material/slider';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -177,7 +174,6 @@ import { VerticeDetailComponent } from './vertice-detail/vertice-detail.componen
 		RouterModule,
 
 		AppRoutingModule,
-		HttpClientModule,
 
 		MatSliderModule,
 		MatSelectModule,
@@ -276,11 +272,6 @@ import { VerticeDetailComponent } from './vertice-detail/vertice-detail.componen
 		{
 			provide: MatDialogRef,
 			useValue: {}
-		},
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: LoggingInterceptorService,
-			multi: true,
 		},
 	],
 })
