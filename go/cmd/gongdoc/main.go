92dd53e8e054c407e53b1a6fac60a607e80232b2
Thomas Peugeot <thomaspeugeot@yahoo.fr>
2023-03-24 10:41:10 +0100

    Allow for specific gong stage per diagram package Fixes #157 - first commit to allow gong compilation

--- go/cmd/gongdoc/main.go
+++ go/cmd/gongdoc/main.go
@@ -72,6 +72,14 @@ func main() {
 	stacksConfig := new(StackConfigs)
 	for _, modelPath := range flag.Args() {
 
+		// gongdocStage := gongdoc_fullstack.NewStackInstance(r, modelPkg.PkgPath)
+		// gongStage := gong_fullstack.NewStackInstance(r, modelPkg.PkgPath)
+		// stacksConfig.Stacks = append(stacksConfig.Stacks, modelPkg.PkgPath)
+
+		gongdocStage := gongdoc_fullstack.NewStackInstance(r, "")
+		gongStage := gong_fullstack.NewStackInstance(r, "")
+		stacksConfig.Stacks = append(stacksConfig.Stacks, "")
+
 		// compute absolute path
 		absPkgPath, _ := filepath.Abs(modelPath)
 
@@ -82,14 +90,6 @@ func main() {
 		// to be removed after fix of [issue](https://github.com/golang/go/issues/57559)
 		gongdoc_models.SetupMapDocLinkRenaming(diagramPackage.Stage_)
 		// end of the be removed
-
-		// gongdocStage := gongdoc_fullstack.NewStackInstance(r, modelPkg.PkgPath)
-		// gongStage := gong_fullstack.NewStackInstance(r, modelPkg.PkgPath)
-		// stacksConfig.Stacks = append(stacksConfig.Stacks, modelPkg.PkgPath)
-
-		gongdocStage := gongdoc_fullstack.NewStackInstance(r, "")
-		gongStage := gong_fullstack.NewStackInstance(r, "")
-		stacksConfig.Stacks = append(stacksConfig.Stacks, "")
 
 		// set up gong structs for diagram package
 		if *setUpRandomNumberOfInstances {
