a80458e3660f30b68552123faa048316c411b7b5
peugeot <thomaspeugeot@yahoo.fr>
2023-06-01 09:00:42 +0200

    svg purifier

--- go/cmd/purifysvg/main.go
+++ go/cmd/purifysvg/main.go
@@ -28,41 +28,59 @@ func main() {
 
 	content := string(data)
 
-	// Find SVG tags and extract the content
-	start := strings.Index(content, "<svg")
-	end := strings.Index(content, "</svg>")
-	if start == -1 || end == -1 {
-		fmt.Println("SVG tags not found in the file.")
-		os.Exit(1)
-	}
-	svgContent := content[start : end+len("</svg>")]
-
-	// Create a decoder
-	decoder := xml.NewDecoder(strings.NewReader(svgContent))
-
 	// Create a buffer for the output
 	var out bytes.Buffer
 	encoder := xml.NewEncoder(&out)
 
-	// Loop over the tokens
-	for {
-		t, err := decoder.Token()
-		if err != nil {
-			break
+	// Write the start of the SVG tag
+	encoder.EncodeToken(xml.StartElement{Name: xml.Name{Local: "svg", Space: "http://www.w3.org/2000/svg"}})
+
+	// Find SVG tags and extract the content
+	for start := strings.Index(content, "<svg"); start != -1; start = strings.Index(content, "<svg") {
+		end := strings.Index(content, "</svg>")
+		if end == -1 {
+			fmt.Println("Unmatched SVG tag.")
+			os.Exit(1)
 		}
 
-		// Check the token type and write non-comment tokens
-		switch t := t.(type) {
-		case xml.Comment:
-			// Skip comments
-			continue
-		default:
-			if err := encoder.EncodeToken(t); err != nil {
-				fmt.Println("Error encoding token:", err)
-				os.Exit(1)
+		// Extract the SVG content, excluding the <svg> and </svg> tags
+		svgContent := content[start+len("<svg>") : end]
+
+		// update content
+		content = content[end+len("</svg>"):]
+
+		// Create a decoder
+		decoder := xml.NewDecoder(strings.NewReader(svgContent))
+
+		// Loop over the tokens
+		for {
+			t, err := decoder.Token()
+			if err != nil {
+				break
+			}
+
+			// Check the token type and write non-comment tokens
+			switch t := t.(type) {
+			case xml.Comment:
+				// Skip comments
+				continue
+			default:
+				if err := encoder.EncodeToken(t); err != nil {
+					fmt.Println("Error encoding token:", err)
+					os.Exit(1)
+				}
 			}
 		}
+
+		// Flush the encoder
+		if err := encoder.Flush(); err != nil {
+			fmt.Println("Error flushing encoder:", err)
+			os.Exit(1)
+		}
 	}
+
+	// Write the end of the SVG tag
+	encoder.EncodeToken(xml.EndElement{Name: xml.Name{Local: "svg"}})
 
 	// End the encoder element
 	if err := encoder.Flush(); err != nil {
