eff92dd7bddd8bc8ccf106e090cabde84d743b33
peugeot <thomaspeugeot@yahoo.fr>
2023-06-06 00:45:26 +0200

    before refactoring

--- go/node2gongdoc/node_cb.go
+++ go/node2gongdoc/node_cb.go
@@ -147,38 +147,6 @@ func (nodeCb *NodeCB) OnAfterDelete(
 	nodeCb.computeNodesConfiguration(stage)
 }
 
-func (nodeCb *NodeCB) FillUpDiagramNodeTree(diagramPackage *gongdoc_models.DiagramPackage) {
-
-	// generate tree of diagrams
-	gongdocTree := (&gongdoc_models.Tree{Name: "gongdoc"}).Stage(nodeCb.diagramPackage.Stage_)
-
-	// add the root of class diagrams
-	diagramPackageNode := (&gongdoc_models.Node{Name: "class diagrams"}).Stage(nodeCb.diagramPackage.Stage_)
-	diagramPackageNode.IsExpanded = true
-	gongdocTree.RootNodes = append(gongdocTree.RootNodes, diagramPackageNode)
-
-	// add one node per class diagram
-	for classdiagram := range *gongdoc_models.GetGongstructInstancesSet[gongdoc_models.Classdiagram](nodeCb.diagramPackage.Stage_) {
-		node := (&gongdoc_models.Node{Name: classdiagram.Name}).Stage(nodeCb.diagramPackage.Stage_)
-
-		node.HasCheckboxButton = true
-		node.HasDeleteButton = true
-		node.HasEditButton = true
-		node.HasDuplicateButton = true
-		diagramPackageNode.Children = append(diagramPackageNode.Children, node)
-
-		// set up the back pointer from the shape to the node
-		classdiagramImpl := new(ClassdiagramImpl)
-		classdiagramImpl.node = node
-		classdiagramImpl.classdiagram = classdiagram
-		classdiagramImpl.nodeCb = nodeCb
-		node.Impl = classdiagramImpl
-	}
-
-	// set callbacks on node updates
-	nodeCb.diagramPackageNode = diagramPackageNode
-}
-
 // computeNodesConfiguration computes both trees
 func (nodeCb *NodeCB) computeNodesConfiguration(gongdocStage *gongdoc_models.StageStruct) {
 
