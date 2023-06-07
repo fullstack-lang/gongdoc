9c44f415e19a0db90888755e61aad86fc3b38b04
peugeot <thomaspeugeot@yahoo.fr>
2023-06-08 01:01:50 +0200

    change the OnAfterCreate method to migrate to a more abstract button model #188 - factoring Classdiagram node creation

--- go/node2gongdoc/fill_up_node_tree.go
+++ go/node2gongdoc/fill_up_node_tree.go
@@ -34,112 +34,8 @@ func FillUpNodeTree(diagramPackage *gongdoc_models.DiagramPackage) {
 
 	// add one node for each diagram
 	for classdiagram := range *gongdoc_models.GetGongstructInstancesSet[gongdoc_models.Classdiagram](diagramPackage.Stage_) {
-		classdiagramNode := (&gongdoc_models.Node{Name: classdiagram.Name}).Stage(diagramPackage.Stage_)
-
+		classdiagramNode := NewClassdiagramNode(classdiagram, diagramPackage, rootOfClassdiagramsNode, treeOfGongObjects)
 		rootOfClassdiagramsNode.Children = append(rootOfClassdiagramsNode.Children, classdiagramNode)
-
-		classdiagramNode.HasCheckboxButton = true
-
-		nodeImplClassdiagram := NewNodeImplClasssiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-		)
-		classdiagramNode.Impl2 = nodeImplClassdiagram
-
-		//
-		// Buttons that are displayed if the node is selected
-		//
-
-		// add draw button
-		drawButton := (&gongdoc_models.Button{
-			Name:      classdiagram.Name + " " + string(BUTTON_draw),
-			Icon:      string(BUTTON_draw),
-			Displayed: true}).Stage(diagramPackage.Stage_)
-		_ = drawButton
-		classdiagramNode.Buttons = append(classdiagramNode.Buttons, drawButton)
-		drawButton.Impl = NewButtonImplClassdiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-			classdiagramNode,
-			nodeImplClassdiagram,
-			BUTTON_draw,
-		)
-
-		// delete button
-		deleteButton := (&gongdoc_models.Button{
-			Name:      classdiagram.Name + " " + string(BUTTON_delete),
-			Icon:      string(BUTTON_delete),
-			Displayed: true}).Stage(diagramPackage.Stage_)
-		_ = deleteButton
-		classdiagramNode.Buttons = append(classdiagramNode.Buttons, deleteButton)
-		deleteButton.Impl = NewButtonImplClassdiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-			classdiagramNode,
-			nodeImplClassdiagram,
-			BUTTON_delete,
-		)
-
-		// file_copy
-		// delete button
-		file_copyButton := (&gongdoc_models.Button{
-			Name:      classdiagram.Name + " " + string(BUTTON_file_copy),
-			Icon:      string(BUTTON_file_copy),
-			Displayed: true}).Stage(diagramPackage.Stage_)
-		_ = file_copyButton
-		classdiagramNode.Buttons = append(classdiagramNode.Buttons, file_copyButton)
-		file_copyButton.Impl = NewButtonImplClassdiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-			classdiagramNode,
-			nodeImplClassdiagram,
-			BUTTON_file_copy,
-		)
-		//
-		// Buttons that are displayed if the node is drawned
-		//
-
-		// add editoff button
-		editoffButton := (&gongdoc_models.Button{
-			Name:      classdiagram.Name + " " + string(BUTTON_edit_off),
-			Icon:      string(BUTTON_edit_off),
-			Displayed: true}).Stage(diagramPackage.Stage_)
-		_ = editoffButton
-		classdiagramNode.Buttons = append(classdiagramNode.Buttons, editoffButton)
-		editoffButton.Impl = NewButtonImplClassdiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-			classdiagramNode,
-			nodeImplClassdiagram,
-			BUTTON_edit_off,
-		)
-
-		// add save button
-		saveButton := (&gongdoc_models.Button{
-			Name:      classdiagram.Name + " " + string(BUTTON_save),
-			Icon:      string(BUTTON_save),
-			Displayed: true}).Stage(diagramPackage.Stage_)
-		_ = saveButton
-		classdiagramNode.Buttons = append(classdiagramNode.Buttons, saveButton)
-		saveButton.Impl = NewButtonImplClassdiagram(
-			diagramPackage,
-			classdiagram,
-			rootOfClassdiagramsNode,
-			treeOfGongObjects,
-			classdiagramNode,
-			nodeImplClassdiagram,
-			BUTTON_save,
-		)
 	}
 
 	computeNodeConfs(diagramPackage.Stage_,
@@ -152,4 +48,120 @@ func FillUpNodeTree(diagramPackage *gongdoc_models.DiagramPackage) {
 	// diagramPackage.Stage_.OnAfterNodeUpdateCallback = nodeCb
 	// diagramPackage.Stage_.OnAfterNodeCreateCallback = nodeCb
 	// diagramPackage.Stage_.OnAfterNodeDeleteCallback = nodeCb
+}
+
+func NewClassdiagramNode(
+	classdiagram *gongdoc_models.Classdiagram,
+	diagramPackage *gongdoc_models.DiagramPackage,
+	rootOfClassdiagramsNode *gongdoc_models.Node,
+	treeOfGongObjects *gongdoc_models.Tree,
+) (classdiagramNode *gongdoc_models.Node) {
+	classdiagramNode = (&gongdoc_models.Node{Name: classdiagram.Name}).Stage(diagramPackage.Stage_)
+
+	rootOfClassdiagramsNode.Children = append(rootOfClassdiagramsNode.Children, classdiagramNode)
+
+	classdiagramNode.HasCheckboxButton = true
+
+	nodeImplClassdiagram := NewNodeImplClasssiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+	)
+	classdiagramNode.Impl2 = nodeImplClassdiagram
+
+	//
+	// Buttons that are displayed if the node is selected
+	//
+
+	// add draw button
+	drawButton := (&gongdoc_models.Button{
+		Name:      classdiagram.Name + " " + string(BUTTON_draw),
+		Icon:      string(BUTTON_draw),
+		Displayed: true}).Stage(diagramPackage.Stage_)
+	_ = drawButton
+	classdiagramNode.Buttons = append(classdiagramNode.Buttons, drawButton)
+	drawButton.Impl = NewButtonImplClassdiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+		classdiagramNode,
+		nodeImplClassdiagram,
+		BUTTON_draw,
+	)
+
+	// delete button
+	deleteButton := (&gongdoc_models.Button{
+		Name:      classdiagram.Name + " " + string(BUTTON_delete),
+		Icon:      string(BUTTON_delete),
+		Displayed: true}).Stage(diagramPackage.Stage_)
+	_ = deleteButton
+	classdiagramNode.Buttons = append(classdiagramNode.Buttons, deleteButton)
+	deleteButton.Impl = NewButtonImplClassdiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+		classdiagramNode,
+		nodeImplClassdiagram,
+		BUTTON_delete,
+	)
+
+	// file_copy
+	// delete button
+	file_copyButton := (&gongdoc_models.Button{
+		Name:      classdiagram.Name + " " + string(BUTTON_file_copy),
+		Icon:      string(BUTTON_file_copy),
+		Displayed: true}).Stage(diagramPackage.Stage_)
+	_ = file_copyButton
+	classdiagramNode.Buttons = append(classdiagramNode.Buttons, file_copyButton)
+	file_copyButton.Impl = NewButtonImplClassdiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+		classdiagramNode,
+		nodeImplClassdiagram,
+		BUTTON_file_copy,
+	)
+	//
+	// Buttons that are displayed if the node is drawned
+	//
+
+	// add editoff button
+	editoffButton := (&gongdoc_models.Button{
+		Name:      classdiagram.Name + " " + string(BUTTON_edit_off),
+		Icon:      string(BUTTON_edit_off),
+		Displayed: true}).Stage(diagramPackage.Stage_)
+	_ = editoffButton
+	classdiagramNode.Buttons = append(classdiagramNode.Buttons, editoffButton)
+	editoffButton.Impl = NewButtonImplClassdiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+		classdiagramNode,
+		nodeImplClassdiagram,
+		BUTTON_edit_off,
+	)
+
+	// add save button
+	saveButton := (&gongdoc_models.Button{
+		Name:      classdiagram.Name + " " + string(BUTTON_save),
+		Icon:      string(BUTTON_save),
+		Displayed: true}).Stage(diagramPackage.Stage_)
+	_ = saveButton
+	classdiagramNode.Buttons = append(classdiagramNode.Buttons, saveButton)
+	saveButton.Impl = NewButtonImplClassdiagram(
+		diagramPackage,
+		classdiagram,
+		rootOfClassdiagramsNode,
+		treeOfGongObjects,
+		classdiagramNode,
+		nodeImplClassdiagram,
+		BUTTON_save,
+	)
+
+	return
 }
