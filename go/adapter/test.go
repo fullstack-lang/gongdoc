525f6d3271934a637a14e4617479b535ba092de2
peugeot <thomaspeugeot@yahoo.fr>
2024-03-02 11:44:15 +0100

    simplification

--- go/adapter/test.go
+++ go/adapter/test.go
@@ -6,12 +6,26 @@ type Foo struct {
 type Bar struct {
 }
 
-func (*Bar) Get() map[*Foo]*Foo {
+func (*Bar) Get() *Foo {
 	return nil
 }
 
-type MapAnyToAny interface {
-	Get() map[any]any
+type I_Venus1 interface {
 }
 
-// var _ MapAnyToAny = &(Bar{})
+type I_Venus2 interface {
+	Get() I_Venus1
+}
+
+type I_Mars1 interface {
+}
+
+type I_Mars2 interface {
+	Get() I_Mars1
+}
+
+var _ I_Venus1 = &(Foo{}) // Foo meets I_Venus1 interface
+var _ I_Mars1 = &(Foo{})  // Foo meets I_Mars1 interface
+
+// var _ I_Venus2 = &(Bar{}) // *Bar does not implement I_Venus2 (wrong type for method Get)
+// var _ I_Mars2 = &(Bar{})  // *Bar does not implement I_Mars2 (wrong type for method Get)
