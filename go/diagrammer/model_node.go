89854af67dd6175c81bc3e0e8106c42dd7bd5cf4
peugeot <thomaspeugeot@yahoo.fr>
2024-03-10 19:08:54 +0100

    Refactor with treeMngt and svgMngt in order to minimize SVG generations #204 - created a file for model_node_impl

--- go/diagrammer/model_node.go
+++ go/diagrammer/model_node.go
@@ -1,7 +1,5 @@
 package diagrammer
 
-import gongtree_models "github.com/fullstack-lang/gongtree/go/models"
-
 type ModelNode interface {
 	GenerateChildren() []ModelNode
 	GetName() string
@@ -9,43 +7,4 @@ type ModelNode interface {
 
 	IsExpanded() bool
 	SetIsExpanded(isExpanded bool)
-}
-
-type ModelNodeImpl struct {
-	diagrammer *Diagrammer
-	modelNode  ModelNode
-}
-
-// OnAfterUpdate implements models.NodeImplInterface
-func (modelNodeImpl *ModelNodeImpl) OnAfterUpdate(stage *gongtree_models.StageStruct, stagedNode *gongtree_models.Node, frontNode *gongtree_models.Node) {
-
-	if frontNode.IsExpanded != stagedNode.IsExpanded {
-		modelNodeImpl.modelNode.SetIsExpanded(!modelNodeImpl.modelNode.IsExpanded())
-
-		// we need to update the stage node because when the tree stage is committed later
-		// the expanded configuration of the node will be correct
-		// if not, the front node will be in the configuration of the stage node
-		// whatever action the end user performs
-		stagedNode.IsExpanded = frontNode.IsExpanded
-	}
-
-	if !stagedNode.IsChecked && frontNode.IsChecked {
-
-		map_ModelNode_Shape := modelNodeImpl.diagrammer.portfolio.AddElement(modelNodeImpl.modelNode)
-
-		stagedNode.IsChecked = frontNode.IsChecked
-		modelNodeImpl.diagrammer.generatePortfolioNodesButtons()
-		modelNodeImpl.diagrammer.computeModelNodeStatus(map_ModelNode_Shape)
-		modelNodeImpl.diagrammer.treeStage.Commit()
-	}
-
-	if stagedNode.IsChecked && !frontNode.IsChecked {
-
-		map_ModelNode_Shape := modelNodeImpl.diagrammer.portfolio.RemoveElement(modelNodeImpl.modelNode)
-
-		stagedNode.IsChecked = frontNode.IsChecked
-		modelNodeImpl.diagrammer.generatePortfolioNodesButtons()
-		modelNodeImpl.diagrammer.computeModelNodeStatus(map_ModelNode_Shape)
-		modelNodeImpl.diagrammer.treeStage.Commit()
-	}
 }
