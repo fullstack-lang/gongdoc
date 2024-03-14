3b015c36130de8005105ddb3d5b89197cf31824b
peugeot <thomaspeugeot@yahoo.fr>
2024-03-14 05:18:52 +0100

    Refactor with treeMngt and svgMngt in order to minimize SVG generations #204 - GongEnumNode

--- go/adapter/gong_note_node.go
+++ go/adapter/gong_note_node.go
@@ -1,8 +1,12 @@
 package adapter
 
 import (
+	"log"
+	"slices"
+
 	gong_models "github.com/fullstack-lang/gong/go/models"
 	"github.com/fullstack-lang/gongdoc/go/diagrammer"
+	gongdoc_models "github.com/fullstack-lang/gongdoc/go/models"
 )
 
 type GongNoteNode struct {
@@ -10,14 +14,59 @@ type GongNoteNode struct {
 	gongNote *gong_models.GongNote
 }
 
+var _ diagrammer.ModelElementNode = &GongNoteNode{}
+
 // RemoveFromDiagram implements diagrammer.ModelElementNode.
 func (gongNoteNode *GongNoteNode) RemoveFromDiagram() {
-	panic("unimplemented")
+	gongdocStage := gongNoteNode.portfolioAdapter.gongdocStage
+	diagramPackage := gongNoteNode.portfolioAdapter.getDiagramPackage()
+	classDiagram := diagramPackage.SelectedClassdiagram
+
+	var noteShape *gongdoc_models.NoteShape
+	shape, ok := gongNoteNode.portfolioAdapter.diagrammer.GetElementNodeDisplayed(gongNoteNode)
+
+	if !ok {
+		log.Fatalln("unknown note shape")
+		return
+	}
+	if noteShape, ok = shape.(*gongdoc_models.NoteShape); !ok {
+		log.Fatalln("not a note shape")
+		return
+	}
+
+	// remove the links of the note shape
+	for _, noteLink := range noteShape.NoteShapeLinks {
+		noteLink.Unstage(gongdocStage)
+		idx := slices.Index(noteShape.NoteShapeLinks, noteLink)
+
+		noteShape.NoteShapeLinks = slices.Delete(noteShape.NoteShapeLinks, idx, idx+1)
+	}
+	idx := slices.Index(classDiagram.NoteShapes, noteShape)
+
+	classDiagram.NoteShapes = slices.Delete(classDiagram.NoteShapes, idx, idx+1)
+	noteShape.Unstage(gongdocStage)
+
+	gongdocStage.Commit()
 }
 
 // AddToDiagram implements diagrammer.ElementNode.
 func (gongNoteNode *GongNoteNode) AddToDiagram() {
-	panic("unimplemented")
+	diagramPackage := gongNoteNode.portfolioAdapter.getDiagramPackage()
+	gongdocStage := gongNoteNode.portfolioAdapter.gongdocStage
+
+	noteShape := (&gongdoc_models.NoteShape{Name: gongNoteNode.GetName()}).Stage(gongdocStage)
+
+	noteShape.Identifier = gongdoc_models.GongStructNameToIdentifier(noteShape.Name)
+
+	noteShape.Body = gongNoteNode.gongNote.Body
+	noteShape.X = 30
+	noteShape.Y = 30
+	noteShape.Width = 240
+	noteShape.Height = 63
+
+	diagramPackage.SelectedClassdiagram.NoteShapes =
+		append(diagramPackage.SelectedClassdiagram.NoteShapes, noteShape)
+	gongdocStage.Commit()
 }
 
 var _ diagrammer.ModelElementNode = &GongNoteNode{}
